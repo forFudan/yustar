06c49974a385ac45392f5d99c4968601e8fc3d49
Yuhao Zhu, PhD, FRM <dr.yuhao.zhu@outlook.com>
2023-10-20 21:59:25 +0200

    Provide full division hints

--- dist/yustar/readme.txt
+++ dist/yustar/readme.txt
@@ -4,7 +4,7 @@
 
 /scheme 中的文件爲 Rime 輸入法各平臺（小狼毫、鼠鬚管）碼表。
 
-使用本方案，需要預先安裝[宇浩輸入法](https://github.com/forFudan/yuhao/releases)，以獲取相關的插件、反查、符號等文件。
+複製所有文件至機器上的 /Rime 文件夾。
 
 請在 default.custom.yaml 文件的 patch/schema_list 列表中手動添加本方案名如下：
 
@@ -24,5 +24,3 @@ patch:
 ## mabiao
 
 /mabiao 中的文件是其他平臺的碼表。
-
-單字全碼表和單字簡碼表在 /mabiao/evaluation 下。
--- 宇浩星陳輸入法打包.py
+++ 宇浩星陳輸入法打包.py
@@ -6,25 +6,48 @@ import shutil
 import os
 from distutils.dir_util import copy_tree
 from distutils.dir_util import remove_tree
+from shutil import copyfile
 
-version = "v3.0.1"
+version = "v3.1.0"
 
 #%%
-try:
-    remove_tree("./dist/yustar")
-except:
-    pass
+for _ in range(2):
+    try:
+        remove_tree("./dist/yustar")
+    except:
+        pass
 
 #%%
 os.makedirs("./dist/yustar")
+
+#%%
+# Copy yustar
 shutil.copyfile("./image/yustar.png", f"./dist/yustar/yustar_{version}.png")
 shutil.copyfile("./beta/readme.md", f"./dist/yustar/readme.txt")
-copy_tree("./beta/mabiao", "./dist/yustar/mabiao")
-copy_tree("./beta/schema", "./dist/yustar/schema")
-copy_tree("./beta/hotfix", "./dist/yustar/hotfix")
+copy_tree("./beta/mabiao/", "./dist/yustar/mabiao/")
+copy_tree("./beta/schema/", "./dist/yustar/schema/")
+copy_tree("./beta/hotfix/", "./dist/yustar/hotfix/")
 
-shutil.make_archive(f"./dist/yustar_{version}", 'zip', "./dist/yustar")
+#%%
+# copy yuhao
+copy_tree("../yuhao/beta/schema/lua/", "./dist/yustar/schema/lua/")
+for file_name in [
+    "default.yaml",
+    "key_bindings.yaml",
+    "punctuation.yaml",
+    "rime.lua",
+    "symbols_yuhao.yaml",
+    "symbols.yaml",
+    "yuhao_pinyin.dict.yaml",
+    "yuhao_pinyin.schema.yaml",
+    "yuhao/yuhao.extended.dict.yaml",
+    "yuhao/yuhao.private.dict.yaml",
+    # "yuhao/yuhao.symbols.dict.yaml",
+]:
+    copyfile(f"../yuhao/beta/schema/{file_name}", f"./dist/yustar/schema/{file_name}")
 
 # %%
-# shutil.make_archive(f"./dist/yustar_{version}_hotfix", 'zip', "./beta/hotfix")
+shutil.make_archive(f"./dist/宇浩星陳_{version}", 'zip', "./dist/yustar")
+# %%
+copyfile(f"./dist/宇浩星陳_{version}.zip", f"../yuhao/dist/宇浩星陳_{version}.zip")
 # %%
