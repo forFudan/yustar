775e4b8787e7f708e61526887ba14db041e48a94
Yuhao Zhu, PhD, FRM <dr.yuhao.zhu@outlook.com>
2023-11-16 23:24:42 +0100

    Update

--- 宇浩星陳輸入法打包.py
+++ 宇浩星陳輸入法打包.py
@@ -1,6 +1,8 @@
 # %%
 
 from datetime import datetime
+import time
+
 # version = datetime.today().strftime('%Y%m%d')
 import shutil
 import os
@@ -8,35 +10,32 @@ from distutils.dir_util import copy_tree
 from distutils.dir_util import remove_tree
 from shutil import copyfile
 
-version = "v3.3.0-beta"
+version = "v3.3.0-beta.2"
 
-#%%
-for _ in range(2):
-    try:
-        remove_tree("./dist/yustar")
-    except:
-        pass
+# %%
+try:
+    remove_tree("./dist/yustar")
+except:
+    os.makedirs("./dist/yustar")
 
-#%%
-os.makedirs("./dist/yustar")
-
-#%%
+# %%
 # Copy yustar
 shutil.copyfile("./image/yustar.png", f"./dist/yustar/yustar_{version}.png")
 shutil.copyfile("./beta/readme.md", f"./dist/yustar/readme.txt")
 copy_tree("./beta/mabiao/", "./dist/yustar/mabiao/")
 copy_tree("./beta/schema/", "./dist/yustar/schema/")
+copy_tree("../yuhao/beta/schema/lua/", "./dist/yustar/hotfix/lua/")
 copy_tree("./beta/hotfix/", "./dist/yustar/hotfix/")
 
-#%%
+# %%
 # copy yuhao
 copy_tree("../yuhao/beta/schema/lua/", "./dist/yustar/schema/lua/")
 for file_name in [
     "default.yaml",
     "key_bindings.yaml",
     "punctuation.yaml",
-    "rime.lua",
-    "symbols_yuhao.yaml",
+    # "rime.lua",
+    "yuhao.symbols.yaml",
     "symbols.yaml",
     "yuhao_pinyin.dict.yaml",
     "yuhao_pinyin.schema.yaml",
@@ -47,8 +46,34 @@ for file_name in [
     copyfile(f"../yuhao/beta/schema/{file_name}", f"./dist/yustar/schema/{file_name}")
 
 # %%
-shutil.make_archive(f"./dist/yuhao_star_{version}", 'zip', "./dist/yustar")
+shutil.make_archive(f"./dist/yuhao_star_{version}", "zip", "./dist/yustar")
 # %%
 copyfile(f"./dist/yuhao_star_{version}.zip", f"../yuhao/dist/yuhao_star_{version}.zip")
 copyfile(f"./dist/yuhao_star_{version}.zip", f"../yuhao/dist/宇浩星陳_{version}.zip")
+# %%
+# %%
+# Remove unrelated files
+for file_name in [
+    "default.yaml",
+    "key_bindings.yaml",
+    "punctuation.yaml",
+    "symbols.yaml",
+    "default.custom.yaml"
+]:
+    os.remove("./dist/yustar/schema/" + file_name)
+
+shutil.make_archive(
+    f"../yuhao/dist/hamster/yuhao_star_{version}", "zip", "./dist/yustar/schema"
+)
+
+# %%
+try:
+    remove_tree("./dist/yustar")
+except:
+    print("Delete incomplete")
+time.sleep(5)
+try:
+    remove_tree("./dist/yustar")
+except:
+    print("Delete incomplete")
 # %%
